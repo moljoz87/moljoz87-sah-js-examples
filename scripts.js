4e636e38b9a2b055d6aabf97b58faab53a576a88
moljoz87 <moljoz87@gmail.com>
2020-04-29 12:47:26 +0200

    third one

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 let name = "Joseph"
 
 console.log(name + "'s apples")
-console.log("Hello World!")
+console.log("Szia Lili! Lássuk az igazságot!")
 
-$("h1").text("Hello szia szevasz, van nalatok terasz?")
+$("h1").text("A nagy szerelem teszt")
+
+
+$('#add-button').click(() => {
+    $('ul').append("<li>Én is szeretlek!! :)</li>")
+})
+
+$('#remove-button').click(() => {
+    $('ul').append("<li>Kapd be, akkor én sem téged! :(</li>")
+})
+
